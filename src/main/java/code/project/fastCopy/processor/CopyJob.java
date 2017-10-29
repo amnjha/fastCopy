99310512e9cf5634bcab3d844af22eb0753946a1
Aman Jha <amanjha22@gmail.com>
2017-10-29 19:33:57 +0530

    Added Logging and copy status update

--- src/main/java/code/project/fastCopy/processor/CopyJob.java
+++ src/main/java/code/project/fastCopy/processor/CopyJob.java
@@ -6,24 +6,26 @@ import code.project.fastCopy.data.DiskType;
 import code.project.fastCopy.data.FileCategory;
 import code.project.fastCopy.data.FileType;
 import code.project.fastCopy.utils.CopyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class CopyJob extends Job<Void> {
 
-    private FileCategory fileCategory;
     private DiskType diskType;
     private File sourceFile;
     private File targetFile;
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(CopyJob.class);
+
     /**
      * @param jobName String
      */
-    public CopyJob(String jobName, DiskType diskType, FileCategory fileCategory, File sourceFile, File targetFile) {
+    public CopyJob(String jobName, DiskType diskType, File sourceFile, File targetFile) {
         super(jobName);
         this.diskType=diskType;
-        this.fileCategory = fileCategory;
         this.sourceFile=sourceFile;
         this.targetFile=targetFile;
     }
@@ -34,6 +36,7 @@ public class CopyJob extends Job<Void> {
      */
     @Override
     public Void doJob() throws Exception {
+        LOGGER.info("Copying File: "+ sourceFile.getAbsolutePath());
         if(!sourceFile.exists())
             throw new RuntimeException("Source File not found", new FileNotFoundException());
 
@@ -42,6 +45,7 @@ public class CopyJob extends Job<Void> {
         FileCategory category= CopyUtils.categorizeFileByFileSize(fileSize,fileType);
         Copy copy= CopyStrategySelector.getCopyObjectByCopyStrategy(CopyUtils.decideCopyStrategy(category, diskType));
         copy.copyFile(sourceFile,targetFile);
+        CopyStatus.updateCopiedBytes(fileSize);
         return null;
     }
 
